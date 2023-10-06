fb59aafdc04c1998d7922688c9e9791d87f17118
CHL07 <Christopher.laukart@gmail.com>
2023-10-06 18:02:03 +0200

    Python lessons

---
+++ Aufgaben.py
@@ -0,0 +1,15 @@
+
+original_string = "Hallo World"
+
+print(original_string)
+print(original_string.lower())
+print(original_string.upper())
+
+firstname ="Christopher"
+lastname ="Laukart"
+
+print("Hallo", firstname, lastname )
+
+print(type(original_string))
+
+
