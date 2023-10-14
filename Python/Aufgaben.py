161618e17d09421dda53738521e8b4af50a1bfe7
CHL07 <Christopher.laukart@gmail.com>
2023-10-14 14:02:54 +0200

    Kapitel 11 Operatoren

--- Python/Aufgaben.py
+++ Python/Aufgaben.py
@@ -18,10 +18,11 @@ print(type(original_string))
 #           Datentypen          #
 #################################
 
-n1 = 2
-n2 = 3
+n1=int(input('Gib mir ine zahl'))
 
-l1 = [(1,2,3,4,5,6,7,8,9)]
+n2=int(input('und noch eine'))
+
+l1 = [1,2,3,4,5,6,7,8,9]
 
 print('2 hoch 3 ist', n1**n2)
 print('2 hoch 3 = ' + str(n1**n2))
@@ -37,21 +38,40 @@ print('2 - 3 ist gleich', n1-n2)
 
 if n1 < n2:
     print('Die zahl',n1 ,' ist kleiner als',n2)
-
+######################################################
 if n1 <= n2:
     print('Die zahl', n1,' ist kleiner oder gleich',n2)
-
+######################################################
 if n1 > n2:
     print (n1,'Ist größer als',n2)
 else:
     print(n1,'ist nicht größer als',n2)
-
-print(n1 != n2)
-print(n1 == n2)
-print(n1 is n2)
-print(n1 is not n2)
-print(n1 in l1)
-print(n1 not in l1)
+######################################################
+if n1 != n2:
+    print('Die zahlen',n1 ,n2,'sind nicht gleich')
+else:
+    print('Die Zahlen',n1 ,n2,'sind gleich')
+######################################################
+if n1 == n2:
+    print('Die Zahlen',n1 ,n2,'sind gleich')
+else:
+    print('Die Zahlen,',n1 ,n2,'sind nicht gleich')
+######################################################
+if n1 is n2:
+    print('Die Zahlen',n1 ,n2,'sind identisch')
+else:
+    print('Die Zahlen,',n1 ,n2,'sind nicht identisch')
+#####################################################    
+if n1 is not n2:
+    print('Die Zahlen',n1 ,n2,'sind nicht identisch')
+else:
+    print('Die Zahlen,',n1 ,n2,'sind identisch')
+######################################################
+if n1 in l1:
+    print('Die zahl',n1,'ist in der liste')
+else:
+    print('Die zahl',n1,'ist nicht in der liste')
+######################################################
 print( not n1)
 print(n1 and n2)
 print(n1 or n2)
