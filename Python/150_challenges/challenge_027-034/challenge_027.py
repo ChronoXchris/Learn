442c03160b578a655349cc427c02cfc1725a3974
CHL07 <Christopher.laukart@gmail.com>
2023-11-08 22:12:57 +0100

    new challenge

---
+++ Python/150_challenges/challenge_027-034/challenge_027.py
@@ -0,0 +1,10 @@
+'''
+Challenge:
+Ask the user to enter a number with lots of
+decimal places. Multiply this number by two and
+display the answer.
+'''
+
+num = float(input("Please enter a number whit decimal places: "))
+num1 = num*2
+print(num1)
---
+++ Python/150_challenges/challenge_027-034/challenge_028.py
@@ -0,0 +1,9 @@
+'''
+Challenge:
+Update program 027 so that it will display the answer to
+two decimal places.
+'''
+
+num = float(input("Please enter a number whit decimal places: "))
+num1 = num*2
+print(round(num1,2))
