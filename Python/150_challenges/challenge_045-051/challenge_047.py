14149422465cf3f6ccbbd3a8807a730347b6b43b
CHL07 <Christopher.laukart@gmail.com>
2023-11-15 19:06:29 +0100

    Added new challenges

---
+++ Python/150_challenges/challenge_045-051/challenge_047.py
@@ -0,0 +1,19 @@
+'''
+Challenge:
+Ask the user to enter a number and then enter
+another number. Add these two numbers together and
+then ask if they want to add another number. If they
+enter “y", ask them to enter another number and keep
+adding numbers until they do not answer “y”. Once the
+loop has stopped, display the total.
+'''
+
+num1= int(input("enter a number: "))
+total = num1
+con = "y"
+while con == "y":
+    num2 = int(input("enter a another number: "))
+    total = total+num2
+    again = input("Do you wane add a number (y/n): ")
+
+print(total)
