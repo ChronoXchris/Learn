3cbe346ffd792e2daa751f338cb236c41125014d
CHL07 <Christopher.laukart@gmail.com>
2023-11-24 21:37:29 +0100

    challenge start

---
+++ Python/150_challenges/challenge_069-076/challenge_069.py
@@ -0,0 +1,13 @@
+'''
+Challenge:
+Create a tuple containing the names of five countries and display the whole tuple. Ask
+the user to enter one of the countries that have been shown to them and then display
+the index number (i.e. position in the list) of that item in the tuple.
+'''
+
+countries_tuple = ("pakistan","germany","france","greece","italy")
+
+print(countries_tuple)
+print()
+countrie = input("enter a country from the list: ")
+print(countrie,"is in the",countries_tuple.index(countrie))
