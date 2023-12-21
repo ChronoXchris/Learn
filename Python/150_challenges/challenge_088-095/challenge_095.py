2859efe872ff3ef0a5e3af6f64a6e46b546772d3
CHL07 <Christopher.laukart@gmail.com>
2023-12-21 15:00:57 +0100

    New challenges every day

--- Python/150_challenges/challenge_088-095/challenge_095.py
+++ Python/150_challenges/challenge_088-095/challenge_095.py
@@ -12,5 +12,24 @@ numbers in the array by the number the
 user entered and display the answers
 shown to two decimal places.
 '''
+tryagain = True
+from array import*
+
+numbers = array('f',[23.45,37.89,56.12,72.34,98.76])
+
+
+
+while tryagain == True:
+    num = int(input("Enter a number between 2 and 5: "))
+    if num < 2 or num > 5 :
+        print("ERROR number is not between 2 and 5")
+        print("Try again")
+    else:
+        tryagain = False
+for i in range(0,5):
+    ans = numbers[i]/num
+    print(round(ans,2))
+
+
 
 
