7207a29e34746a1edba91ddd6c94684c81e6a439
CHL07 <Christopher.laukart@gmail.com>
2023-12-28 20:23:06 +0100

    New challenges every day

--- Python/150_challenges/challenge_111-117/challenge_111.py
+++ Python/150_challenges/challenge_111-117/challenge_111.py
@@ -6,3 +6,17 @@ Create a .csv file that will store the following data. Call it “Books.csv”.
 import csv
 
 file = open("Books.csv","w")
+newRecord = "To Kill A Mockingbird, Harper Lee, 1960"
+file.write(str(newRecord))
+newRecord = "A brief History of Time, Stephen Hawking, 1988"
+file.write(str(newRecord))
+newRecord = "The Great Gatsby, F.Scott Fitzgerald, 1922"
+file.write(str(newRecord))
+newRecord = "The Man Who Mistook His Wife for a Hat, Oliver Sacks, 1985"
+file.write(str(newRecord))
+newRecord = "Pride and Prejudice, Jane Austen, 1813"
+file.write(str(newRecord))
+file.close()
+
+
+
