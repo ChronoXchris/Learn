6c78db58b5f40c9674c490c744fc951653669c35
CHL07 <Christopher.laukart@gmail.com>
2024-07-03 19:12:23 +0200

    100 day python(day 1)

---
+++ Udemy/python_100_days/Band_generator.py
@@ -0,0 +1,7 @@
+"""
+A generator who will create a band name for you 
+"""
+print("Welcome to the Band Name Generator.")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet name?\n")
+print("Your band name could be",city+" "+pet)
