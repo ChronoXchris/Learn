2b09aa1d5f7eab8d9d7c1154153c616a8fff0e60
CHL07 <Christopher.laukart@gmail.com>
2025-01-31 18:27:23 +0100

    Day 21

--- Udemy/python_100_days/Day 20/snake.py
+++ Udemy/python_100_days/Day 20/snake.py
@@ -10,23 +10,23 @@ RIGHT = 0
 class Snake:
 
     def __init__(self):
-        self.segments = []
+        self.snake_body = []
         self.create_snake()
-        self.head = self.segments[0]
+        self.head = self.snake_body[0]
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            new_body = Turtle("square")
+            new_body.color("white")
+            new_body.penup()
+            new_body.goto(position)
+            self.snake_body.append(new_body)
 
     def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
+        for seg_num in range(len(self.snake_body) - 1, 0, -1):
+            new_x = self.snake_body[seg_num - 1].xcor()
+            new_y = self.snake_body[seg_num - 1].ycor()
+            self.snake_body[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
@@ -44,3 +44,4 @@ class Snake:
     def right(self):
         if self.head.heading() != LEFT:
             self.head.setheading(RIGHT)
+            
